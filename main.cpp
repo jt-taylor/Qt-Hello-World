655145fa94a93e9322f4ab9ae3b996dbb979e51d
Jackson Tyler Taylor <jtttaylorjackson@gmail.com>
2022-03-15 15:40:25 -0700

    HelloWorld , this is hello world of qt applications

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QPushButton button ("Hello world !");
+    button.show();
+
+    return a.exec();
+}
